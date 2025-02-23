ed3b2e80be18a0ae506c35701a21e3f5ea499a69
Francoi-H <herbyfr64@gmail.com>
2025-02-23 13:47:06 -0500

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -160,10 +160,11 @@ const Navbar = ( ) => {
     <Nav>
     <NavContainer>
         <NavLogo to="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
-        </a>
-        </NavLogo>
+  <a href="/" style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
+    <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+  </a>
+</NavLogo>
+
         <MobileIcon>
             <FaBars
                 onClick={() => {
